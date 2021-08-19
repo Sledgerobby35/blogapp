dd929e8c600925fd03d947206de32815ff76f2f1
Sledgerobby35 <sledge.robby35@gmail.com>
2021-08-19 17:11:34 -0500

    add categories to object

--- src/main/java/com/codeup/blogapp/data/Post.java
+++ src/main/java/com/codeup/blogapp/data/Post.java
@@ -1,20 +1,25 @@
 package com.codeup.blogapp.data;
 
+import java.util.Collection;
+
 public class Post {
 
     private Long id;
     private String title;
     private String content;
     private User user;
+    private Collection<Category> categories;
 
     public Post() {
     }
 
-    public Post(Long id, String title, String content, User user) {
+    public Post(Long id, String title, String content, User user,
+                Collection<Category> categories) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.user = user;
+        this.categories = categories;
     }
 
     public Long getId() {
@@ -47,5 +52,13 @@ public class Post {
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public Collection<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Collection<Category> categories) {
+        this.categories = categories;
     }
 }
